a9b49b84569a1b89f75bc1dcd4e8d6101e23e701
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2026-01-26 20:33:59 +0530

    update

--- main.ts
+++ main.ts
@@ -4,10 +4,23 @@ const client = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY_TEMP'],
 });
 
-const response = await client.responses.create({
+// const response = await client.responses.create({
+//   model: 'gpt-5-mini',
+//   instructions: 'You are hulk from marvel, respond as if we are in avengers endgame movie',
+//   input: 'Hi!!!!! YOOOOOOOOOOOOO! WELCOME BACK!',
+// });
+
+// console.log(response.output_text);
+//
+
+const completion = await client.chat.completions.create({
   model: 'gpt-5-mini',
-  instructions: 'You are hulk from marvel, respond as if we are in avengers endgame movie',
-  input: 'Hi!!!!! YOOOOOOOOOOOOO! WELCOME BACK!',
+  messages: [
+    { role: 'developer', content: 'You are hulk' },
+    { role: 'user', content: 'Hello! who are you?' },
+  ],
 });
 
-console.log(response.output_text);
+console.log("Completion object: ", completion);
+console.log("-------------------------")
+console.log(completion.choices[0].message.content);
