fb9f5f76dbb0486027bf4614668e11e1689b74bc
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2026-02-03 21:29:45 +0530

    update

--- ink-ui.tsx
+++ ink-ui.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render, Box, Text } from 'ink';
 
+
+type Message = {
+  role: 'ai' | 'human' | 'system';
+  text: string;
+}
+
 function App() {
+
+  const [messages, setMessages] = useState<Message[]>([
+    { role: 'ai', text: 'Hello!' }
+  ]);
+
   return (
-    <Box backgroundColor="green" borderColor="red" border={3}>
-      <Text>Chat App</Text>
+    <Box flexDirection="column" borderColor="red" borderLeft={true} padding={1}>
+      <ChatHistory messages={messages} />
+    </Box>
+  );
+}
+
+function ChatHistory(props: { messages: Message[] }) {
+  const { messages } = props;
+
+  return (
+    <Box flexDirection="column" paddingBottom={1}>
+      {messages.map((msg, i) => (
+        <Box key={i} marginBottom={1}>
+          <Box borderColor={msg.role === 'ai' ? 'blue' : 'green'} borderStyle={'round'}>
+            <Text>{msg.role} : {msg.text}</Text>
+          </Box>
+        </Box>
+      ))}
     </Box>
   );
 }
